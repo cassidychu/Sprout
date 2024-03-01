0aac3c9c2e98cdd82d53b9ee0e19ce58607c072c
albhandari <alexbhandari37@gmail.com>
2024-02-29 19:56:01 -0800

    Database

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+import { useState } from 'react';
+import { doc, setDoc } from "firebase/firestore"; 
+import { db } from './firebaseConfig';
+
+
 
 export default function App() {
+
+  const [username, setName] = useState('') 
+  const [email, setEmail] = useState('') 
+
+  function create(){
+
+    setDoc(doc(db, "users", "LA"), {
+      username: username,
+      email: email,
+    }).then(()=> {
+      console.log('data submitted');
+    }).catch((error)=>{
+      console.log(error);
+    });
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Firebase Data Posting</Text>
+
+      <TextInput value={username} onChangeText={(username) => {setName(username)}} placeholder="Username" style={styles.textBoxes}></TextInput>
+      <TextInput value={email} onChangeText={(email) => {setEmail(email)}} placeholder="Email" style={styles.textBoxes}></TextInput>
+      <button onClick = { create }>Submit Button</button>
     </View>
   );
 }
